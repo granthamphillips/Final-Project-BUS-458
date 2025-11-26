23b7388195b8bc8da3d6b23c6af87f347cf74676
granthamphillips <granthamphillips@gmail.com>
2025-11-25 19:54:34 -0500

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -9,7 +9,7 @@ import numpy as np
 # Load the trained model
 with open("my_model_scaler.pkl", "rb") as file:
     model = pickle.load(file)
-    sclaer = bundle["scaler"]
+    scaler = bundle["scaler"]
 
 # Title for the app
 # st.title("Loan Approval")
