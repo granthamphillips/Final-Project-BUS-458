993c107be08cc75a94192cba277953e2447db78a
granthamphillips <granthamphillips@gmail.com>
2025-11-25 19:37:38 -0500

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -25,7 +25,6 @@ Requested_Loan_Amount = st.slider("Loan Amount (Requested_Loan_Amount)", min_val
 Monthly_Housing_Payment = st.slider("Housing Payment", min_value=0.0, max_value=1000000.0, step=1000.0)
 Monthly_Gross_Income = st.slider("Monthly Income)", min_value=0.0, max_value=1000000.0, step=1000.0)
 FICO_score = st.slider("FICO Score", min_value=300, max_value=850, value =700, step=10, key="fico_slider")
-derog = st.number_input("Derogatory Reports (DEROG)", min_value=0, max_value=15, step=1)
 Ever_Bankrupt_or_Foreclose = st.selectbox("Ever Bankrupt or Foreclose)", options=list(range(0, 1)))  # Options from 0 to 1
 
 
@@ -97,9 +96,9 @@ if st.button("Evaluate Loan"):
 
     # Display result
     if prediction == 1:
-        st.write("The prediction is: **Bad Loan** 🚫")
+        st.write("The prediction is: **Approved** ✅")
     else:
-        st.write("The prediction is: **Good Loan** 💲")
+        st.write("The prediction is: **Denied** 🚫")
 
 
 
