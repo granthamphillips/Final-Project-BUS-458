49b1167e76bddae810ff314afee7c16b877422c4
granthamphillips <granthamphillips@gmail.com>
2025-12-02 12:30:52 -0500

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -24,7 +24,7 @@ st.markdown(
 st.header("Enter Loan Applicant's Details")
 
 # Input fields for numeric values
-Requested_Loan_Amount = st.slider("Loan Amount])", min_value=5000.0, max_value=2500000.0, step=1000.0)
+Requested_Loan_Amount = st.slider("Loan Amount", min_value=5000.0, max_value=2500000.0, step=1000.0)
 Monthly_Housing_Payment = st.slider("Housing Payment", min_value=0.0, max_value=50000.0, step=100.0)
 Monthly_Gross_Income = st.slider("Monthly Income)", min_value=0.0, max_value=20000.0, step=1000.0)
 FICO_score = st.slider("FICO Score", min_value=300, max_value=850, value =700, step=10, key="fico_slider")
